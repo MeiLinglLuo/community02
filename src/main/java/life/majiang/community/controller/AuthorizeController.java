e5ffbd2e801f3f880360b860e751b046be446224
MeiLinglLuo <464119137@qq.com>
2020-07-10 18:59:21 +0800

    commit03

---
+++ src/main/java/life/majiang/community/controller/AuthorizeController.java
@@ -0,0 +1,35 @@
+package life.majiang.community.controller;
+
+
+import life.majiang.community.dto.AccessTokenDTO;
+import life.majiang.community.dto.GithubUser;
+import life.majiang.community.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class AuthorizeController {
+
+    @Autowired
+    private GithubProvider githubProvider;
+
+
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name="code") String code,
+                           @RequestParam(name="state") String state){
+        AccessTokenDTO accessTokenDTO =  new AccessTokenDTO();
+        accessTokenDTO.setClient_id("17ee172d255f8b99b3c9");
+        accessTokenDTO.setClient_secret("89d891813de25f858be4b88af2ac4c3989ff61b3");
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setRedirect_uri("http://localhost:8887/callback");
+        accessTokenDTO.setState(state);
+
+        String accessToken = githubProvider.gitAccessToken(accessTokenDTO);
+        GithubUser user = githubProvider.getUser(accessToken);
+        System.out.println(user.getName());
+
+        return "index";
+    }
+}
